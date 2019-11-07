ad7d27036176df94347606a3ebc733c88f7f098d
Paul Ronga <paul.ronga@laposte.net>
2019-11-07 21:09:25 +0100

    move calculateur de distance

--- distances/js/select.js
+++ distances/js/select.js
@@ -1,87 +1,192 @@
 'use strict';
 
 
+$.getJSON( "json/distances.json", function(data) {
+  // console.log(data)
 
-var inputFields = $('.chosen-value');
-var dropdowns = $('.value-list');
-var dropdownArray = [].concat(document.querySelectorAll('li'));
-var dropdownItems = dropdownArray[0];
+  // pour jQuery
+  var inputFields = $('.chosen-value');
+  var dropdowns = $('.value-list');
 
-var valueArray = [];
-var slugs = [];
-$('.unique-list li').each(function(){
-    valueArray.push($(this).text());
-    slugs.push($(this).text().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')); // todo: sans accents etc.
-});
+  // accès rapide aux li en vanilla js
+  var dropdownArray = [].concat(document.querySelectorAll('li'));
+  var dropdownItems = dropdownArray[0];
 
-var closeDropdown = function closeDropdown() {
-  dropdown.classList.remove('open');
-};
+  // valeurs
+  var valueArray = [];
+  var slugs = [];
 
-const dropdown = document.querySelector('.value-list');
-// dropdown.classList.add('open');
-// inputField.focus();
 
-inputFields.each(function(i, d){
-  $(this).on('input', function () {
-    var targetValues = $(this).data('values');
-    var targetDropdown = $('.value-list.' + targetValues);
-    targetDropdown.addClass('open');
 
-    var inputValue = $(this).val().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  $('.unique-list li').each(function(){
+      valueArray.push($(this).text());
 
-    var targetDropdownItems = targetDropdown.find('li');
+      slugs.push($(this).text().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')); // todo: sans accents etc.
+  });
 
-    var valueSubstring = undefined;
-    if (inputValue.length > 0) {
-      for (var j = 0; j < valueArray.length; j++) {
-
-        if (!(slugs[j].includes(inputValue))) {
-          targetDropdownItems[j].classList.add('closed');
-        } else {
-          targetDropdownItems[j].classList.remove('closed');
-        }
-      }
-    } else {
-      for (var i = 0; i < dropdownItems.length; i++) {
-        targetDropdownItems[i].classList.remove('closed');
-      }
+  /*
+  // debug: pour checker que tout matche entre json et li
+  for (var i = 0; i < data.length; i++) {
+    if(valueArray.indexOf( data[i]['from'] ) < 0){
+      console.log(data[i]['from'])
     }
 
+  }
+  for (var i = 0; i < data.length; i++) {
+    if(valueArray.indexOf( data[i]['to'] ) < 0){
+      console.log(data[i]['to'])
+    }
+  }
+  */
 
-  })
-});
+  /*
+  Affichage des résultats
+  */
 
-$('.value-list li').on('click', function(e){
-  $(this).parent().prev().val($(this).text())
-  $('.value-list li').addClass('closed');
-  e.stopPropagation();
-});
+  function displaySchedule(){
+    var from = $('.chosen-value.from').val();
+    var to = $('.chosen-value.to').val();
 
-inputFields.each(function(i, d){
-  $(this).on('focus', function () {
-    $(this).attr('placeholder', $(this).data('placeholder'));
+    if(from && to){
 
-    var targetValues = $(this).data('values');
-    var targetDropdown = $('.value-list.' + targetValues);
-    targetDropdown.addClass('open');
+      var found = false;
+
+      for (var i = 0; i < data.length; i++) {
+        if( (data[i]['from'] == from) && (data[i]['to'] == to) ){
+          found = true;
+          console.log(data[i]);
+
+          var minutes = data[i]['distance'];
+          var time;
+          if (minutes > 60){
+            var hours = Math.floor(minutes / 60);
+            minutes = minutes % 60;
+            if (hours > 1){
+              time = hours + ' heures et ' + minutes + ' minutes'
+            }else{
+              time = 'une heure et ' + minutes + ' minutes'
+            }
+          }else{
+            time = minutes + ' minutes'
+          }
+
+          $('#schedule-result div.error').text('');
+
+          $('#schedule-result div.time .prepend').text('Le trajet dure ')
+          $('#schedule-result div.time .bigfat').text(time)
+          $('#schedule-result div.time .append').text('dans ce sens.')
+          $('#schedule-result div.time .reverse').text('Voir le trajet en sens inverse')
+
+          // $('#schedule-result div.price').text(data[i]['price'] + ' CHF pour un billet de 2e classe plein tarif')
+        }
+      }
+      if(!found){
+        $('#schedule-result div.time *').text('');
+
+        if(from == to){
+          $('#schedule-result div.error').text('Merci de choisir deux stations différentes.')
+        }else{
+          $('#schedule-result div.error').text('Aucune information trouvée.')
+        }
+      }
+    }
+  }
+
+  /*
+  Menu déroulant avec filtre
+  */
+
+  inputFields.each(function(i, d){
+    $(this).on('input', function () {
+      var targetValues = $(this).data('values');
+      var targetDropdown = $('.value-list.' + targetValues);
+      targetDropdown.addClass('open');
+
+      var inputValue = $(this).val().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+
+      var targetDropdownItems = targetDropdown.find('li');
+
+      var valueSubstring = undefined;
+      if (inputValue.length > 0) {
+        for (var j = 0; j < valueArray.length; j++) {
+
+          if (!(slugs[j].includes(inputValue))) {
+            targetDropdownItems[j].classList.add('closed');
+          } else {
+            targetDropdownItems[j].classList.remove('closed');
+          }
+        }
+      } else {
+        for (var i = 0; i < dropdownItems.length; i++) {
+          targetDropdownItems[i].classList.remove('closed');
+        }
+      }
 
 
-    targetDropdown.find('li').each(function(){
-      $(this).removeClass('closed')
     })
   });
-});
 
-inputFields.each(function(i, d){
-  $(this).on('blur', function () {
-    var targetValues = $(this).data('values');
-    var targetDropdown = $('.value-list.' + targetValues);
-    targetDropdown.addClass('closed');
+  // Choix d’une valeur
+  $('.value-list li').on('click', function(e){
+    $(this).parent().prev().val($(this).text())
+    $('.value-list li').addClass('closed');
+
+    displaySchedule();
+
+    e.stopPropagation();
   });
-});
-$('.interactive-container').click(function() {
-  console.log('x')
-  $('.value-list').addClass('closed');
-  // $('.value-list li').addClass('closed');
+
+  $('.reverse').click(function(e){
+    var from = $('.chosen-value.from').val();
+    var to = $('.chosen-value.to').val();
+    $('.chosen-value.from').val(to);
+    $('.chosen-value.to').val(from);
+
+    displaySchedule();
+    e.stopPropagation();
+  })
+
+  inputFields.each(function(i, d){
+
+    // pour permettre de fermer quand clic dehors
+    $(this).on('click', function (e) {
+      e.stopPropagation();
+    });
+
+    // focus dans l'input
+    $(this).on('focus', function (e) {
+
+      // On invite à taper
+      // $(this).attr('placeholder', "");
+      $(this).select();
+
+      var targetValues = $(this).data('values');
+      var targetDropdown = $('.value-list.' + targetValues);
+      targetDropdown.addClass('open');
+
+
+      targetDropdown.find('li').each(function(){
+        $(this).removeClass('closed')
+      })
+    });
+  });
+
+
+
+  inputFields.each(function(i, d){
+    $(this).on('blur', function () {
+      var targetValues = $(this).data('values');
+      var targetDropdown = $('.value-list.' + targetValues);
+      targetDropdown.addClass('closed');
+    });
+  });
+  $('body').click(function() {
+    $('.value-list').addClass('closed');
+    $('.value-list li').addClass('closed');
+  });
+
+})
+.fail(function() {
+  console.log( "Erreur chargement des arrêts" );
+  $('#schedule-result div.error').text('Erreur lors du chargement des données.')
 });
