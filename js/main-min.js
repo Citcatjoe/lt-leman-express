b46e31f1bb8ae60e6eb25886132a1b864ef2afb0
Paul Ronga <paul.ronga@laposte.net>
2019-11-07 21:05:17 +0100

    add minified js

--- js/main-min.js
+++ js/main-min.js
@@ -1,1 +1,1 @@
-jQuery(document).ready(function(o){o("img.lazy").lazyload({effect:"fadeIn"}),o(window).width()>1200&&o(".zoom").click(function(){zoom.to({element:o(this)[0]})}),o(".scroll-indicator").click(function(){o("html, body").animate({scrollTop:o("section:eq(0)").offset().top-80},1e3)})});
+jQuery(document).ready((function(o){o("img.lazy").lazyload({effect:"fadeIn"}),o(window).width()>1200&&o(".zoom").click((function(){zoom.to({element:o(this)[0]})})),o(".scroll-indicator").click((function(){o("html, body").animate({scrollTop:o("section:eq(0)").offset().top-80},1e3)}))}));
